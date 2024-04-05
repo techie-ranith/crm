178557c84a24c47ec111556a869a7e0d69f5b37d
techie-ranith <ranith2k3@gmail.com>
2024-04-05 12:12:22 +0530

    login update

--- client/src/component/login/Login.js
+++ client/src/component/login/Login.js
@@ -1,4 +1,3 @@
-'use client'
 import React, { useState } from 'react';
 import img2 from './2.png';
 import img3 from './3.png';
@@ -6,45 +5,45 @@ import img1 from './1.png';
 import { FaUser } from "react-icons/fa";
 import { RiLockPasswordFill } from "react-icons/ri";
 
-  const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      const loginData = {
-        username: username,
-        password: password
-      };
-  
-      try {
-        const response = await fetch('api/userAuth', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(loginData)
-        });
-  
-        if (!response.ok) {
-          throw new Error('Login failed');
-        }
-  
-        const data = await response.json();
-        if (data.success) {
-          alert(data.message); // Display success message
-          window.location.href = 'http://localhost:3000/adduser'; // Redirect user on success
-        } else {
-          setErrorMessage(data.message); // Set error message
-        }
-      } catch (error) {
-        console.error('Login error:', error.message);
-        setErrorMessage('Something went wrong. Please try again later.');
-      }
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const loginData = {
+      username: username,
+      password: password
     };
-  
+
+    try {
+      const response = await fetch('api/userAuth', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Login failed');
+      }
+
+      const data = await response.json();
+      if (data.success) {
+        alert(data.message); // Display success message
+        window.location.href = 'http://localhost:3000/adduser'; // Redirect user on success
+      } else {
+        setErrorMessage(data.message); // Set error message
+      }
+    } catch (error) {
+      console.error('Login error:', error.message);
+      setErrorMessage('Login Unsucessful! Please try again.');
+    }
+  };
+
   return (
     <div className='flex '>
       {/* Background images */}
@@ -56,43 +55,39 @@ import { RiLockPasswordFill } from "react-icons/ri";
       {/* Login form */}
       <div className="flex flex-col items-center justify-center p-4 ml-80">
         <img className="w-40 h-32" src={img1} alt="" />
-        <button className="h-16 my-4 font-semibold text-white bg-teal-800 w-96 rounded-3xl focus:outline-none hover:bg-teal-600">
-             Login
-        </button>
-
-        <form className="w-full mt-16 space-y-4">
-        <div className="relative">
+        {errorMessage && <div className="mb-4 text-red-500">{errorMessage}</div>}
+        <form onSubmit={handleSubmit} className="w-full mt-16 space-y-4">
+          <div className="relative">
             <input
-                type="text"
-                placeholder="Username"
-                className="block w-full h-16 p-2 pl-10 mb-8 text-xl font-bold placeholder-black placeholder-opacity-50 border-none rounded-xl bg-gradient-to-r from-teal-400 to-gray-200"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="block w-full h-16 p-2 pl-10 mb-8 text-xl font-bold placeholder-black placeholder-opacity-50 border-none rounded-xl bg-gradient-to-r from-teal-400 to-gray-200"
             />
             <FaUser className="absolute mt-2 text-gray-400 left-3 top-4" />
-        </div>
-        <div className="relative">
-          <input
-            type="password"
-            placeholder="Password"
-            className="block w-full h-16 p-2 pl-10 mb-8 text-xl font-bold placeholder-black placeholder-opacity-50 border-none rounded-xl bg-gradient-to-r from-teal-400 to-gray-200"
-          />
-          <RiLockPasswordFill className="absolute mt-2 text-gray-400 left-3 top-4" />
-        </div>
-
-          {/* Remember Me and Forgot Password */}
-          <div className='flex items-center'>
+          </div>
+          <div className="relative">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="block w-full h-16 p-2 pl-10 mb-8 text-xl font-bold placeholder-black placeholder-opacity-50 border-none rounded-xl bg-gradient-to-r from-teal-400 to-gray-200"
+            />
+            <RiLockPasswordFill className="absolute mt-2 text-gray-400 left-3 top-4" />
+          </div>
+          <div className="flex items-center">
             <label className="flex items-center cursor-pointer">
               <input type="checkbox" className="w-4 h-4 mr-2 form-checkbox" />
               <span>Remember Me</span>
             </label>
-
             <label className="flex ml-auto cursor-pointer hover:text-blue ">
               <span>Forgot Password</span>
             </label>
           </div>
-
-          {/* Log in button */}
           <div className="flex items-center justify-center">
-            <button className="p-2 mt-4 bg-teal-800 border border-gray-300 hover:bg-teal-600 hover:text-black w-36 h-14 rounded-xl">
+            <button type="submit" className="p-2 mt-4 bg-teal-800 border border-gray-300 hover:bg-teal-600 hover:text-black w-36 h-14 rounded-xl">
               Log in
             </button>
           </div>
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+328f8721270a21be1e4309ad9fe63bcf93b7b844")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
